f9858a78da415c5eec65b3e5213fe17aeddb0a3b
“job-kiptoo-dev” <kiptoojob73@gmial.com>
2026-02-24 14:06:27 +0300

    setting up drizzle config and scripts for drizzle db:up,generate,migrate,studio

---
+++ src/db/db.ts
@@ -0,0 +1,17 @@
+import { drizzle } from "drizzle-orm/node-postgres"
+
+import * as schema from "@/db/scheme"
+
+import { Pool } from "pg"
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+  max: 10,
+  idleTimeoutMillis: 30000,
+})
+
+
+
+export const db = drizzle(pool, { schema, casing: 'snake_case' })
+
+

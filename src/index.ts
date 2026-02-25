316927db0b9e7c0080d5af01e0d89756642f9d6d
“job-kiptoo-dev” <kiptoojob73@gmial.com>
2026-02-25 10:55:13 +0300

    Learnt something  new  open-cli have push a new user to the db

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { Hono } from 'hono'
+import { auth } from './lib/auth'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app
+  .on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw))
+  .get('/', (c) => {
+    return c.text('Hello Hono!')
+  })
 
 app.get('/:id', (c) => {
   const id = c.req.param('id')

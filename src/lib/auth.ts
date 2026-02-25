4e3eb2a7b05c4047674c2839abcb91225db0422d
“job-kiptoo-dev” <kiptoojob73@gmial.com>
2026-02-25 17:32:53 +0300

    I dint use mailgun but Resend came in clatch I managed to use Email Verification

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,7 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { Pool } from "pg";
 import { openAPI } from "better-auth/plugins"
+import { sendEmail } from "@/helper/helper";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -13,5 +14,21 @@ export const auth = betterAuth({
   ],
   emailAndPassword: {
     enabled: true,
-  }
+    autoSignIn: true,
+    minPasswordLength: 8,
+    maxPasswordLength: 20,
+    requireEmailVerification: true
+  },
+  emailVerification: {
+    sendOnSignUp: true,
+    autoSignInAfterVerification: true,
+    sendVerificationEmail: async ({ user, url }) => {
+      await sendEmail.emails.send({
+        from: "Acme <onboarding@resend.dev>",
+        to: user.email,
+        subject: "Email Verification",
+        html: `Click the link to verify your email: ${url}`,
+      });
+    },
+  },
 });

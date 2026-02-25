48fbb591fb23eca1962fb08299ca5cb880932959
“job-kiptoo-dev” <kiptoojob73@gmial.com>
2026-02-25 10:55:13 +0300

    Learnt something  new  open-cli have push a new user to the db

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ bun install
 To run:
 ```sh
 bun run dev
-```
 
+```
+```
 open http://localhost:3000
+
+## **Open CLI** may refer to general methods for opening a Command Line Interface on a specific operating system
+`Open-cli`
+
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,11 +2,15 @@ import { db } from "@/db/db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { Pool } from "pg";
+import { openAPI } from "better-auth/plugins"
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg"
   }),
+  plugins: [
+    openAPI(),
+  ],
   emailAndPassword: {
     enabled: true,
   }
